c51b6929312ae8553b427950bba4e508de810f1d
darcielu <s60042apple@hotmail.com>
2017-08-21 23:58:27 +0800

    加入Table

---
+++ ToDoSample/Models/ToDoContext .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ToDoSample.Models
+{
+    public class ToDoContext : DbContext
+    {
+        public DbSet<ToDoModel> ToDoModels { get; set; }
+
+        public ToDoContext()
+            : base("name=DefaultConnection")
+        {
+
+        }
+    }
+}
---
+++ ToDoSample/Models/ToDoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoSample.Models
+{
+    public class ToDoModel
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public bool IsCompleted { get; set; }
+        [Required]
+        public DateTime CreateTime { get; set; }
+    }
+}
